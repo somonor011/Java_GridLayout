aecc5a91000d1610e06b61d2a7f738c95c9d29bf
Somonor Hong <150505791+somonor011@users.noreply.github.com>
2024-07-05 16:20:24 +0700

    Add files via upload

---
+++ Java_GirdLayout/src/java_girdlayout/Main.java
@@ -0,0 +1,33 @@
+
+package java_girdlayout;
+
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class Main {
+
+    public static void main(String[] args) {
+       JFrame frame = new JFrame();
+       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       frame.setSize(500, 500);
+       frame.setLayout(new GridLayout(3,3,10,10)); // GridLayout(row,column,horizontal,vertical)
+       // horizontal​ is column spacing
+       // vertical is row spacing
+       
+       // Create button
+       frame.add(new JButton("1"));
+       frame.add(new JButton("2"));
+       frame.add(new JButton("3"));
+       frame.add(new JButton("4"));
+       frame.add(new JButton("5"));
+       frame.add(new JButton("6"));
+       frame.add(new JButton("7"));
+       frame.add(new JButton("8"));
+       frame.add(new JButton("9"));
+       
+       
+       frame.setVisible(true);
+    }
+
+}
